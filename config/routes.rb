e048ef4bedb897a42cb5599077d2d1c13a4f8a53
Chadwick Flores <chadwickflores@gmail.com>
2016-05-19 16:45:17 -0500

    add courses controller CRUD action and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :sessions => "sessions", :registrations => "registrations" }
 
-  resources :schools do
-  end
+  resources :courses
+  resources :schools
 
   get "/my_current_user" => "users#my_current_user"
   match 'users/:id' => 'users#update_user', via: [:patch]
